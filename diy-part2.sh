64e5f5bf2e4f00409dd465354d1312e300a665a3
Jays <8566666@gmail.com>
2024-03-17 11:19:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,18 +9,28 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-# 回退
-# git reset --hard a2a0e5b9f6ce420398487ea29a85b0b0c49c9087
+function merge_package(){
+    repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
+    pkg=`echo $2 | rev | cut -d'/' -f 1 | rev`
+    git clone --depth=1 --single-branch $1
+    mv $2 package/custom/
+    rm -rf $repo
+}
+function drop_package(){
+    find package/ -follow -name $1 -not -path "package/custom/*" | xargs -rt rm -rf
+}
+function merge_feed(){
+    if [ ! -d "feed/$1" ]; then
+        echo >> feeds.conf.default
+        echo "src-git $1 $2" >> feeds.conf.default
+    fi
+    ./scripts/feeds update $1
+    ./scripts/feeds install -a -p $1
+}
+rm -rf package/custom; mkdir package/custom
 
-# openssl-1.1.1
-rm -rf package/libs/openssl
-# svn co https://github.com/istoreos/istoreos/trunk/package/libs/openssl package/libs/openssl
-git clone https://github.com/istoreos/istoreos.git package/istoreos 
-cp -r package/istoreos/package/libs/openssl package/libs/
-rm -rf package/istoreos
-
-# 修改默认IP
-sed -i 's/192.168.1.1/192.168.1.110/g' package/base-files/files/bin/config_generate
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # poweroff
 git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
@@ -36,17 +46,39 @@ git clone -b 18.06 https://github.com/SpeedPartner/luci-theme-argon-18.06-patch.
 sed -i '41,59d' feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer.htm
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 
+#微信推送
+rm -rf feeds/kenzo/luci-app-wechatpush
+rm -rf feeds/luci/applications/luci-app-serverchan
+git clone -b openwrt-18.06 https://github.com/tty228/luci-app-serverchan.git feeds/luci/applications/luci-app-serverchan
+
+#luci-app-adguardhome
+rm -rf feeds/kenzo/luci-app-adguardhome
+git clone https://github.com/Zane-E/luci-app-adguardhome.git feeds/kenzo/luci-app-adguardhome
+
+#filebrowser
+rm -rf feeds/kenzo/luci-app-filebrowser
+# git clone -b 18.06 https://github.com/xiaozhuai/luci-app-filebrowser feeds/kenzo/luci-app-filebrowser
+merge_package https://github.com/Lienol/openwrt-package openwrt-package/luci-app-filebrowser
+
+#mosdns
+rm -rf feeds/kenzo/luci-app-mosdns
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/luci/applications/luci-app-mosdns
+merge_package https://github.com/sbwml/luci-app-mosdns luci-app-mosdns
+merge_package https://github.com/sbwml/luci-app-mosdns luci-app-mosdns/mosdns
+
 # 修改主题背景
-cp -f $GITHUB_WORKSPACE/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+# cp -f $GITHUB_WORKSPACE/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+
+# xfsprogs
+cp -f $GITHUB_WORKSPACE/patch/xfsprogs/Makefile feeds/packages/utils/xfsprogs/Makefile
+
+# v2dat
+cp -f $GITHUB_WORKSPACE/patch/v2dat/Makefile feeds/packages/utils/v2dat/Makefile
 
 # golang1.22
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-sed -i 's/ +libopenssl-legacy//g' feeds/helloworld/shadowsocksr-libev/Makefile
-
-# curl8.5
-# cp -f $GITHUB_WORKSPACE/patch/net/curl/Makefile feeds/packages/net/curl/Makefile
-# cp -f $GITHUB_WORKSPACE/patch/net/curl/200-no_docs_tests.patch feeds/packages/net/curl/patches/200-no_docs_tests.patch
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
